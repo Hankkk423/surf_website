71a8f00ad5b29f9e6715fc4a4564ffe19da1d79f
Hank <yenhank112@gmail.com>
2023-10-22 20:46:45 -0700

    Add s-gridbox in index.html

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,6 @@
 
 
-
+// For Scroll Bar
 $(document).ready(function () {
     const scrollBar = $("#scroll-bar");
     const leftArrow = $("#left-arrow");
@@ -12,5 +12,33 @@ $(document).ready(function () {
 
     rightArrow.click(function () {
         scrollBar.animate({ scrollLeft: scrollBar.scrollLeft() + 200 }, "slow");
+    });
+});
+
+
+// $(document).ready(function () {
+//     $('.dropdown-toggle').click(function () {
+//         $('.dropdown-menu').toggle();
+//     });
+// });
+
+
+// For Tab Bar
+$(document).ready(function () {
+    // Initially, set the first tab as active
+    $(".tab-item[data-tab='tab1']").addClass("active");
+    $(".tab-panel#tab1").addClass("active");
+
+    // Handle tab click events
+    $(".tab-item").click(function () {
+        const tabId = $(this).data("tab");
+
+        // Remove the "active" class from all tabs and panels
+        $(".tab-item").removeClass("active");
+        $(".tab-panel").removeClass("active");
+
+        // Add the "active" class to the clicked tab and panel
+        $(this).addClass("active");
+        $(`.tab-panel#${tabId}`).addClass("active");
     });
 });
