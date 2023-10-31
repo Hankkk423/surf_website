00689de89c7f33bfa0c2a8fb59b67540737fdd91
Hank Yen <yenhank112@gmail.com>
2023-10-31 16:56:40 -0700

    Add chart from 2 to 3 in index.html

--- assets/js/script.js
+++ assets/js/script.js
@@ -80,42 +80,37 @@ $(document).ready(function () {
     // Handle hover events for each bar
     $chartBars.on('mouseenter', function () {
         const barId = $(this).attr('id');
-
         console.log('barId: ', barId);
-
-
-        // Calculate the position of the tooltip based on the hovered bar
-        const barX = parseInt($(this).attr('x'));
-        console.log('barX: ', barX);
-
-
-        // Get the position of the currently hovered bar
-        const barPosition = $(this).position().left;
-        console.log('barPosition: ', barPosition);
-
 
         // Extract the numerical part from the id attribute
         const barNumber = parseInt(barId.replace('bar', ''));
         console.log('barNumber: ', barNumber);
 
 
-        // Read the data-toolpos attribute
-        const barDataToolPos = parseFloat($(this).data('toolpos'));
-        console.log('barDataToolPos: ', barDataToolPos);
-
-
-
         ///
         // Calculate the width of the chart area (1/3 of the available space)
         const chartAreaWidth = $('.s-wavechart .chart-area').width();
         console.log('chartAreaWidth: ', chartAreaWidth);
+
+        const tooltipWidth = $tooltipCard.width();
+        console.log('tooltipWidth: ', tooltipWidth);
+
+        const tooltipContainerWidth = $tooltipContainer.width();
+        console.log('tooltipContainerWidth: ', tooltipContainerWidth);
+
+
+        // Calculate the maximum left position for the tooltip card to stay within the screen
+        const maxLeft = tooltipContainerWidth - tooltipWidth;
         
         // Calculate the width of each segment (for 8 bars in 1/3 space)
         const segmentWidth = chartAreaWidth / 8;
         console.log('segmentWidth: ', segmentWidth);
         
         // Calculate the position of the tooltip based on the bar number and screen size
-        const tooltipX = (barNumber - 1) * segmentWidth;
+        let tooltipX = (barNumber - 1) * segmentWidth;
+
+        // Ensure the tooltip card doesn't go beyond the screen edge
+        tooltipX = Math.min(tooltipX, maxLeft);
         ///
 
 
@@ -167,42 +162,37 @@ $(document).ready(function () {
     // Handle hover events for each bar
     $chartBars.on('mouseenter', function () {
         const barId = $(this).attr('id');
-
         console.log('barId: ', barId);
-
-
-        // Calculate the position of the tooltip based on the hovered bar
-        const barX = parseInt($(this).attr('x'));
-        console.log('barX: ', barX);
-
-
-        // Get the position of the currently hovered bar
-        const barPosition = $(this).position().left;
-        console.log('barPosition: ', barPosition);
-
 
         // Extract the numerical part from the id attribute
         const barNumber = parseInt(barId.replace('bar', ''));
         console.log('barNumber: ', barNumber);
 
 
-        // Read the data-toolpos attribute
-        const barDataToolPos = parseFloat($(this).data('toolpos'));
-        console.log('barDataToolPos: ', barDataToolPos);
-
-
-
         ///
         // Calculate the width of the chart area (1/3 of the available space)
         const chartAreaWidth = $('.s-windchart .chart-area').width();
         console.log('chartAreaWidth: ', chartAreaWidth);
+
+        const tooltipWidth = $tooltipCard.width();
+        console.log('tooltipWidth: ', tooltipWidth);
+
+        const tooltipContainerWidth = $tooltipContainer.width();
+        console.log('tooltipContainerWidth: ', tooltipContainerWidth);
+
+
+        // Calculate the maximum left position for the tooltip card to stay within the screen
+        const maxLeft = tooltipContainerWidth - tooltipWidth;
         
         // Calculate the width of each segment (for 8 bars in 1/3 space)
         const segmentWidth = chartAreaWidth / 8;
         console.log('segmentWidth: ', segmentWidth);
         
         // Calculate the position of the tooltip based on the bar number and screen size
-        const tooltipX = (barNumber - 1) * segmentWidth;
+        let tooltipX = (barNumber - 1) * segmentWidth;
+
+        // Ensure the tooltip card doesn't go beyond the screen edge
+        tooltipX = Math.min(tooltipX, maxLeft);
         ///
 
 
