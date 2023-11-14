6bde76585612da4c65aba8d32ecc5ac366d31539
Hank Yen <yenhank112@gmail.com>
2023-11-13 17:14:49 -0800

    Add read json function for 3 chart in index.html

--- assets/js/script.js
+++ assets/js/script.js
@@ -101,29 +101,20 @@ $(document).ready(function () {
         waveData["waves_height-surface"].slice(16, 24)
     ];
 
-    // SVG chart dimensions
-    // const svgWidth = 400;
-    // const svgHeight = 200;
-    const svgWidth = 379;
-    const svgHeight = 116;
 
     // Bar chart dimensions and styling
     const barSpacing = 10; // Space between bars
-    const maxBarHeight = 126;
+    const maxBarHeight = 116;
     const chartColor = "blue";
 
     days.forEach((dayData, index) => {
-        // Create an SVG element for each day
-        const svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
-        svg.setAttribute("width", svgWidth);
-        svg.setAttribute("height", svgHeight);
-
-        // Append the SVG element to the corresponding chart area div
-        const chartArea = document.querySelector('.s-wavechart .chart-area-' + (index + 1));
-        chartArea.appendChild(svg);
+        const waveSvgId = "wave-svg-" + (index + 1);
+        const waveSvg = document.getElementById(waveSvgId);
+        // const svgWidth = contain.clientWidth/3;
+        const waveSvgWidth = waveSvg.clientWidth;
 
         const numBars = dayData.length;
-        const barWidth = (svgWidth - (numBars - 1) * barSpacing - 20) / numBars; // Subtract 20 for spacing
+        const barWidth = (waveSvgWidth - (numBars - 1) * barSpacing - 20) / numBars; // Subtract 20 for spacing
 
         // Generate bars based on JSON data for the day
         dayData.forEach((height, barIndex) => {
@@ -142,7 +133,88 @@ $(document).ready(function () {
             rect.setAttribute("id", "bar" + (barIndex + 1));
             rect.setAttribute("class", "each-bar");
 
-            svg.appendChild(rect);
+            waveSvg.appendChild(rect);
+        });
+    });
+
+});
+
+
+
+
+// For Wind Chart SVG
+$(document).ready(function () {
+    // JSON data for wind heights
+    const windData = {
+        "winds_speed": [
+            0.48507989799255413,
+            0.26870220935624706,
+            0.2703030639024085,
+            0.24349211339402088,
+            0.6203252020247032,
+            0.26038180148397605,
+            0.4315050809330188,
+            0.27556301455407833,
+            0.303867199134936,
+            0.41512659009309827,
+            0.44268415763772095,
+            0.5351906810551451,
+            0.2635027952210286,
+            0.8995599735764346,
+            0.44556973158857316,
+            0.660346565678719,
+            0.37544826669897446,
+            0.29394593671650354,
+            0.35637926401676573,
+            0.5439146657037593,
+            0.508319194030306,
+            0.278684008291131,
+            0.3103244275564241,
+            0.23673892866988103,
+        ]
+    };
+
+    // Create an array to separate data into daily segments
+    const days = [
+        windData["winds_speed"].slice(0, 8),
+        windData["winds_speed"].slice(8, 16),
+        windData["winds_speed"].slice(16, 24)
+    ];
+
+
+    // Bar chart dimensions and styling
+    const barSpacing = 10; // Space between bars
+    const maxBarHeight = 70;
+    const chartColor = "blue";
+
+    days.forEach((dayData, index) => {
+        const windSvgId = "wind-svg-" + (index + 1);
+        const windSvg = document.getElementById(windSvgId);
+        console.log("test: ", windSvg);
+        // const svgWidth = contain.clientWidth/3;
+        const windSvgWidth = windSvg.clientWidth;
+
+        const numBars = dayData.length;
+        const barWidth = (windSvgWidth - (numBars - 1) * barSpacing - 20) / numBars; // Subtract 20 for spacing
+
+        // Generate bars based on JSON data for the day
+        dayData.forEach((height, barIndex) => {
+            const x = barIndex * (barWidth + barSpacing) + 10; // Add 10 for spacing on the left
+            const barHeight = height * maxBarHeight;
+            const y = maxBarHeight - barHeight;
+            const rect = document.createElementNS("http://www.w3.org/2000/svg", "rect");
+            rect.setAttribute("x", x);
+            rect.setAttribute("y", y);
+            rect.setAttribute("width", barWidth);
+            rect.setAttribute("height", barHeight);
+            rect.setAttribute("fill", chartColor);
+
+            console.log("barIndex: ", barIndex);
+
+            rect.setAttribute("id", "bar" + (barIndex + 1));
+            rect.setAttribute("class", "each-bar");
+
+            windSvg.appendChild(rect);
         });
     });
 
@@ -169,15 +241,12 @@ $(document).ready(function () {
 
     // Handle hover events for each bar
     $chartBars.on('mouseenter', function () {
+        // Extract the numerical part from the id attribute
         const barId = $(this).attr('id');
         console.log('barId: ', barId);
-
-        // Extract the numerical part from the id attribute
         const barNumber = parseInt(barId.replace('bar', ''));
         console.log('barNumber: ', barNumber);
 
-
-        ///
         // Calculate the width of the chart area (1/3 of the available space)
         const chartAreaWidth = $('.s-wavechart .chart-area').width();
         console.log('chartAreaWidth: ', chartAreaWidth);
@@ -201,25 +270,13 @@ $(document).ready(function () {
 
         // Ensure the tooltip card doesn't go beyond the screen edge
         tooltipX = Math.min(tooltipX, maxLeft);
-        ///
-
-
-
-        //const tooltipX = barDataToolPos; // Adjust as needed
         console.log('tooltipX: ', tooltipX);
-
 
         // Move and show the tooltip
         $tooltipCard.css({ left: tooltipX + 'px' }).show();
 
         // Update the currently hovered bar
         hoveredBar = barId;
-
-
-        ///////////
-        // $(this).css({ 'stroke': 'black', 'stroke-width': '2' });
-
-
 
     });
 
@@ -232,6 +289,9 @@ $(document).ready(function () {
         }
     });
 });
+
+
+
 
 
 // For Tooltip Card for Wind
@@ -251,15 +311,12 @@ $(document).ready(function () {
 
     // Handle hover events for each bar
     $chartBars.on('mouseenter', function () {
+        // Extract the numerical part from the id attribute
         const barId = $(this).attr('id');
         console.log('barId: ', barId);
-
-        // Extract the numerical part from the id attribute
         const barNumber = parseInt(barId.replace('bar', ''));
         console.log('barNumber: ', barNumber);
 
-
-        ///
         // Calculate the width of the chart area (1/3 of the available space)
         const chartAreaWidth = $('.s-windchart .chart-area').width();
         console.log('chartAreaWidth: ', chartAreaWidth);
@@ -283,26 +340,13 @@ $(document).ready(function () {
 
         // Ensure the tooltip card doesn't go beyond the screen edge
         tooltipX = Math.min(tooltipX, maxLeft);
-        ///
-
-
-
-        //const tooltipX = barDataToolPos; // Adjust as needed
         console.log('tooltipX: ', tooltipX);
-
 
         // Move and show the tooltip
         $tooltipCard.css({ left: tooltipX + 'px' }).show();
 
         // Update the currently hovered bar
         hoveredBar = barId;
-
-
-        ///////////
-        // $(this).css({ 'stroke': 'black', 'stroke-width': '2' });
-
-
-
     });
 
     $chartBars.on('mouseleave', function () {
@@ -317,3 +361,121 @@ $(document).ready(function () {
 
 
 
+
+
+
+
+// For Tide Chart
+$(document).ready(function () {
+
+    // Sample JSON data (replace this with your actual JSON data)
+    const jsonData = {
+        "data": [
+            { "x": 0, "y": 200, "Date": "", "Time": "" },
+            { "x": 1, "y": 100, "Date": "", "Time": "" },
+            { "x": 54, "y": 68, "Date": "11/11", "Time": "05:19" },
+            { "x": 111, "y": 141, "Date": "11/11", "Time": "11:03" },
+            { "x": 171, "y": 55, "Date": "11/11", "Time": "17:01" },
+            { "x": 237, "y": 168, "Date": "11/11", "Time": "23:40" },
+            { "x": 300, "y": 63, "Date": "11/12", "Time": "05:59" },
+            { "x": 356, "y": 137, "Date": "11/12", "Time": "11:34" },
+            { "x": 414, "y": 49, "Date": "11/12", "Time": "17:24" },
+            { "x": 482, "y": 180, "Date": "11/13", "Time": "00:11" },
+            { "x": 547, "y": 61, "Date": "11/13", "Time": "06:38" },
+            { "x": 601, "y": 133, "Date": "11/13", "Time": "12:06" },
+            { "x": 659, "y": 44, "Date": "11/13", "Time": "17:50" },
+            { "x": 719, "y": 100, "Date": "", "Time": "" },
+            { "x": 720, "y": 200, "Date": "", "Time": "" },
+        ]
+    };
+
+    // Set up the SVG container
+    const svg = d3.select(".s-tidechart .tide-chart-svg");
+
+    // Calculate scaling factor based on SVG width and height
+    const svgWidth = parseInt(svg.style("width"));
+    const scaleFactorX = svgWidth / 720; // Assuming the original range is 0 to 720
+    const svgHeight = parseInt(svg.style("height"));
+    const scaleFactorY = svgHeight / 200; // Assuming the original range is 0 to 200
+    
+
+    // Apply scaling to x-coordinates
+    jsonData.data.forEach(d => {
+        d.x = d.x * scaleFactorX;
+        d.y = d.y * scaleFactorY;
+    });
+
+
+    // Create a smooth line generator
+    const line = d3.line()
+        .x(d => d.x)
+        .y(d => d.y)
+        // .curve(d3.curveBasis); // Use curveBasis for a smooth curve
+        .curve(d3.curveCardinal.tension(0.1)); // Adjust the tension parameter (0.7 is just an example)
+
+    // Draw the path
+    svg.append("path")
+        .data([jsonData.data])
+        .attr("d", line)
+        .attr("fill", "rgba(185, 211, 223, 0.4)");
+
+    // Draw the path (for "chart line") stroke="#171717" stroke-width="1.5px" fill="none"
+    svg.append("path")
+        .data([jsonData.data])
+        .attr("d", line)
+        .attr("stroke", "#171717")
+        .attr("stroke-width", "1.5px")
+        .attr("fill", "none");
+
+
+    // Draw vertical strokes for each point
+    const verticalStrokes = svg.selectAll("g.vertical-stroke")
+        // .data(jsonData.data.filter(d => d.x !== 0 && d.x !== 1 && d.x !== 719 && d.x !== 720))
+        .data(jsonData.data.slice(2, -2)) // Exclude first two and last two elements
+        .enter()
+        .append("g")
+        .attr("class", "vertical-stroke");
+
+    verticalStrokes.append("line")
+        .attr("x1", d => d.x)
+        .attr("x2", d => d.x)
+        .attr("y1", d => d.y) // Extracting y-coordinate from the curved line path
+        .attr("y2", svgHeight) // Adjust the y2 value based on your chart height
+        .attr("stroke", "#171717")
+        .attr("stroke-width", "1.5px")
+        .attr("opacity", "0.4");
+
+
+    // Add text labels with formatted DateTime
+    verticalStrokes.append("text")
+        .attr("x", d => d.x)
+        .attr("y", d => d.y - 16) // Adjust the y-coordinate for text positioning
+        .attr("text-anchor", "middle")
+
+        .text(d => {
+            const date = d.Date;
+            const time = d.Time;
+            return `${date} ${time}`;
+        })
+
+        .attr("fill", "#171717")
+        .attr("font-size", "10px")
+        .attr("font-weight", "700");
+
+
+    // Add text labels with formatted DateTime
+    verticalStrokes.append("text")
+        .attr("x", d => d.x)
+        .attr("y", d => d.y - 6) // Adjust the y-coordinate for text positioning
+        .attr("text-anchor", "middle")
+
+        .text(d => {
+            const tideheight = ((100 - (d.y / scaleFactorY)) / 100).toFixed(2);  // "/scaleFactorY" bc it "*scaleFactorY" above to fit svg height
+            return `${tideheight} m`;
+        })
+
+        .attr("fill", "#171717")
+        .attr("font-size", "10px")
+        .attr("font-weight", "700");
+
+});
